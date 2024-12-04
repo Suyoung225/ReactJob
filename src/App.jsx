1db3b3d1a5007ab46b76ea5e9a3a21fff0b174fa
suyoungyun <8904su@gmail.com>
2024-12-04 15:51:02 +0900

    Feat: add jobspage and change a tag to Link tag

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage/>} />
     </Route>
   )
 )
