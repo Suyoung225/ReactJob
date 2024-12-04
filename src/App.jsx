b3593898a80fee29e319737da9e8deff011a3ec4
suyoungyun <8904su@gmail.com>
2024-12-04 15:40:26 +0900

    Feat: add router and layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
   )
-}
+)
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App
