481354c09328dc450c436f6103dc226e84d534de
suyoungyun <8904su@gmail.com>
2024-12-10 15:23:50 +0900

    Feat: pass function as prop

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const AddJobPage = () => {
-  const [title, setTitle] = useState('Full-Time')
-  const [type, setType] = useState('')
+const AddJobPage = ({ addJobSubmit }) => {
+  const [title, setTitle] = useState('')
+  const [type, setType] = useState('Full-Time')
   const [location, setLocation] = useState('')
   const [description, setDescription] = useState('')
   const [salary, setSalary] = useState('Under $50K')
@@ -12,6 +13,28 @@ const AddJobPage = () => {
   const [contactEmail, setContactEmail] = useState('')
   const [contactPhone, setContactPhone] = useState('')
 
+  const navigate = useNavigate()
+
+  const SubmitForm = (e) => {
+    e.preventDefault()
+    const newJob = {
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company : {
+        name : companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone
+      }
+    }
+
+    addJobSubmit(newJob)
+    return navigate('/jobs');
+  }
+
   return (
     <>
     <section className="bg-indigo-50">
@@ -19,7 +42,7 @@ const AddJobPage = () => {
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <form>
+          <form onSubmit={SubmitForm}>
             <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div className="mb-4">
